5166d0b8b592bcc71938e11e91984e10e958de2e
Ramonrpro <122824092+Ramonrpro@users.noreply.github.com>
2023-09-30 10:19:58 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#form-sorteador').addEventListener('submit', function(evento){
+        evento.preventDefault()
+        let numeroMaximo = document.querySelector('#numero-maximo').value
+        numeroMaximo = parseInt(numeroMaximo)
+
+        let numeroAleatorio = Math.random() * numeroMaximo
+        numeroAleatorio = Math.floor(numeroAleatorio +1)
+        document.querySelector('#resultado-valor').textContent = numeroAleatorio
+        document.querySelector('.resultado').style.display = 'block'
+    })
+})
